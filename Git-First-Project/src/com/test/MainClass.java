cc09636bf7feab1d58d783cfb2a4d6135332874f
BPNasir-git <programmernasiruddin@gmail.com>
2020-08-26 14:09:32 +0600

    4th Commit

--- Git-First-Project/src/com/test/MainClass.java
+++ Git-First-Project/src/com/test/MainClass.java
@@ -3,11 +3,21 @@ package com.test;
 public class MainClass {
 	public static void main(String args[]) {
 		String fullname=getFullName("Nasir","Khan PG");
+		
+		int getTotalAmount=getTotalAmount(10,20);
+		
 		System.out.println("Full Name "+fullname);
+	}
+
+	private static int getTotalAmount(int i, int j) {
+		// TODO Auto-generated method stub
+		return i+j;
 	}
 
 	private static String getFullName(String string, String string2) {
 		// TODO Auto-generated method stub
 		return string+" "+string2;
 	}
+	
+	
 }
