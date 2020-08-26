6ffab92cb0645885d85b89ba2852cd84c93187f1
BPNasir-git <programmernasiruddin@gmail.com>
2020-08-26 13:30:15 +0600

    first commit from nasit

--- Git-First-Project/src/com/test/MainClass.java
+++ Git-First-Project/src/com/test/MainClass.java
@@ -1,5 +1,13 @@
 package com.test;
 
 public class MainClass {
+	public static void main(String args[]) {
+		String fullname=getFullName("Nasir","Uddin");
+		System.out.println("Full Name "+fullname);
+	}
 
+	private static String getFullName(String string, String string2) {
+		// TODO Auto-generated method stub
+		return string+" "+string2;
+	}
 }
